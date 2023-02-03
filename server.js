c8a51048bbed0ebe78c97232b45d9148ab3ecc38
radis <luobobo@radiss-iMac.local>
2023-02-03 22:53:00 +0800

    fix: deploy

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ import fs from 'fs';
 import Koa from 'koa';
 import Router from '@koa/router';
 import staticFiles from 'koa-static';
-  
-const PORT = 3000  
+
+const PORT = 3000;
 
 const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx) => {
   ctx.type = 'text/html';
-  ctx.body = fs.readFileSync("./dist/index.html");
+  ctx.body = fs.readFileSync('./build/index.html');
 });
 
 app.use(router.routes());
-app.use(staticFiles('./dist'));
+app.use(staticFiles('./build'));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+  console.log('Listening on port %s. Visit http://localhost:%s/', PORT, PORT);
 });
